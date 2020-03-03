1c836c63c099c3275f7e13a151373f0f5b0dd112
imran1761 <imran.k1761@gmail.com>
2020-03-04 01:22:59 +0530

    initial command

--- process.php
+++ process.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 date_default_timezone_set("Asia/Kolkata");
 
 $mysqli = new mysqli('localhost', 'root', '', 'document') or die(mysqli_error(mysqli));
@@ -9,20 +10,20 @@ if (isset($_POST['save'])) {
 	
 	$mysqli->query ("INSERT INTO users (name, location) values('$name', '$location')") or die($mysqli->error); 
 
-	$_SESSION['message'] = "Record have been saved!";
-	$_SESSION['msg_type'] = "success";
+	$_SESSION["message"] = "Record have been saved!";
+	$_SESSION["msg_type"] = "success";
 
-	header("location: intex.php");
+	header("location: index.php");
 }
 
 if (isset($_GET['delete'])) {
 	$id = $_GET['delete'];
 	$mysqli->query ("DELETE FROM users WHERE id = $id") or die($mysqli->error);
 
-	$_SESSION['message'] = "Record have been deleted";
-	$_SESSION['msg_type'] = "danger";
+	$_SESSION["message"] = "Record have been deleted";
+	$_SESSION["msg_type"] = "danger";
 
-	header("location: intex.php");
+	header("location: index.php");
 }
     if (isset($_POST['search'])) {
     	  $from_date = $_POST['from_date'];
