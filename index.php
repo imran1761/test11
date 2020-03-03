8ddca54e8dfec237b83f8796712fc97a0ff9beb5
imran1761 <imran.k1761@gmail.com>
2020-03-04 01:25:35 +0530

    initial command

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>index</title>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<?php include 'process.php'; ?>
+
+
+	<?php
+		if (isset($_SESSION['message'])): ?>
+
+		<div class="alert alert-<?$_SESSTION['mgs_type']?>">
+			<?php
+			echo $_SESSTION['message'];
+			unset($_SESSTION['message']);
+			?>
+		</div>
+	<?php endif ?>
+	<div class="container">
+
+	<?php
+		$mysqli = new mysqli('localhost', 'root', '', 'document') or die(mysqli_error(mysqli));
+		$result = $mysqli->query("select * from users") or die($mysqli->error);
+
+		//pre_r($res->fetch_assoc());
+		?>
+
+		<div class="row justify-content-center">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Location</th>
+						
+						<th colspan="2">Action</th>
+					</tr>
+				</thead>
+		<?php
+
+			while ($row = $result->fetch_assoc()):
+			?>
+				<tr>
+					<td><?php echo $row['name']; ?></td>
+					<td><?php echo $row['location']; ?></td>
+					<td>
+						<a href="intex.php?edit=<?php echo $row['id']; ?>"
+							class="btn btn-info">Edit</a>
+						<a href="process.php?delete=<?php echo $row['id']; ?>"/* need add date and time*/
+							class="btn btn-danger">Delet</a>
+					</td>
+				</tr>
+			<?php endwhile; ?>
+		</table>
+	</div>
+
+		<?php
+		function pre_r( $array ){
+			echo '<pre>';
+			print_r($array);
+			echo '</pre>';
+		}
+		?>
+	<div class="row justify-content-center">
+	<form action="process.php" method="post">
+		<div class="form-group">
+		<label>Name</label>
+		<input type="text" name="name" class="form-control" value="Enter Your Name">
+		</div>
+		<div class="form-group">
+		<label>location</label>
+		</div>
+		<input type="text" name="location" class="form-control" value="Enter Your location">
+		<div class="form-group">
+		<button type="submit" name="save" class="btn btn-primary"> save </button>
+		</div>
+	</div>
+	</form>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>login</title>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<div class="form-group">
+        <input class="form-control"  maxlength="50" placeholder="User Name" name="user_name" type="text" autofocus>
+    </div>
+        	<div class="form-group">
+                <input class="form-control" maxlength="50" placeholder="Password" name="password"
+                        type="password" >
+            </div>
+            <div class="form-group">
+                <input class="btn btn-outline btn-info btn-block" type="submit"
+                       name="submit" value="Login"/>
+
+</body>
+</html>
